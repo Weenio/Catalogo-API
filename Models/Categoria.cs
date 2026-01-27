22170049b5ace92888a9b8bc4a73261182c9e6cf
NioParabellum <guidavinicius@outlook.com>
2026-01-26 21:02:07 -0300

    first commit

---
+++ Models/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalogo_API.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? NomeCategoria { get; set; }
+    public string? ImgUrl { get; set; }
+}
